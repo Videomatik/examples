6d576419b7c88101be823dc7aeb72c8d7eb73fb3
Vitor Santana <vitorsanusp@gmail.com>
2022-10-03 21:00:07 -0300

    feat(node-js/pooling): add logic to handle HTTP request errors

--- node-javascript/pooling-example/src/index.js
+++ node-javascript/pooling-example/src/index.js
@@ -11,32 +11,39 @@ const videomatik = new VideomatikAPI({ apiKey: process.env.VIDEOMATIK_API_KEY })
 
 // Returns the video request when fully rendered
 const renderVideo = async (customJSON) => {
-  const { id: videoRequestId } = await videomatik.createVideoRequest({
-    templateId: 'oferta-varejo-nujyuua',
-    customJSON,
-    compositionId: 'default'
-  })
+  try {
+    const newVideoRequest = await videomatik.createVideoRequest({
+      // templateId: 'tdc-business-trilha-vertical-ic0w1eb',
+      templateId: 'oferta-varejo-nujyuua',
+      customJSON,
+      compositionId: 'default'
+    })
 
-  return new Promise((resolve, reject) => {
-    const intervalId = setInterval(async () => {
-      const videoRequest = await videomatik.getOneVideoRequest(videoRequestId)
-      const videoState = videoRequest.renderJob.state
+    return new Promise((resolve, reject) => {
+      const intervalId = setInterval(async () => {
+        const videoRequest = await videomatik.getOneVideoRequest(newVideoRequest.id)
+        const videoState = videoRequest.renderJob.state
 
-      if (videoState === 'error') {
-        clearInterval(intervalId)
-        const error = new Error(`An error ocurred while rendering video "${videoRequestId}".`)
-        reject(error)
-      } else if (videoState === 'finished') {
-        clearInterval(intervalId)
-        resolve(videoRequest)
-      } else {
-        console.log(`Video "${videoRequestId}" is at state "${videoState}".`)
-      }
-    }, 10000) // Every 10 seconds, check the video status
-  })
+        if (videoState === 'error') {
+          clearInterval(intervalId)
+          const error = new Error(`An error ocurred while rendering video "${newVideoRequest.id}".`)
+          reject(error)
+        } else if (videoState === 'finished') {
+          clearInterval(intervalId)
+          resolve(videoRequest)
+        } else {
+          console.log(`Video "${newVideoRequest.id}" is at state "${videoState}".`)
+        }
+      }, 10000) // Every 10 seconds, check the video status
+    })
+  } catch (error) {
+    throw new Error(`Video request creation failed with status code ${error.response.status} and message: "${error.response.data.message}"`)
+  }
 }
 
 const makeVideos = async () => {
+  let foundErrorsDuringVideosCreation = false
+
   for (const product of products) {
     const customJSON = getCustomJSON(baseJSON, product)
 
@@ -52,10 +59,15 @@ const makeVideos = async () => {
       console.log('Download completed!\n')
     } catch (err) {
       console.error(err.message + '\n')
+      foundErrorsDuringVideosCreation = true
     }
   }
 
-  console.log('All videos were downloaded successfully! Enjoy your videos :)')
+  if (foundErrorsDuringVideosCreation) {
+    console.log('There were errors while creating the videos. Please, refer to the previous messages to know which ones.')
+  } else {
+    console.log('All videos were downloaded successfully! Enjoy your videos :)')
+  }
 }
 
 makeVideos()
